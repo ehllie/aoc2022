6f21e65a21adffd94a358dfaafc3b83cdb136442
Elizabeth Paź <me@ehllie.xyz>
2022-11-30 15:34:37 +0100

    Add line parsing function

--- lib/Inputs.hs
+++ lib/Inputs.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE TemplateHaskell #-}
 
-module Inputs (inputFor, linesFor) where
+module Inputs (inputFor, linesFor, logParse) where
 
-import TH
+import TH (compileEnv)
 
+import Data.List (partition)
+import Data.Maybe (fromJust, isNothing)
 import System.Directory (getCurrentDirectory)
 import System.FilePath ((<.>), (</>))
 
@@ -15,3 +17,13 @@ inputFor prob = readFile $ inputsDir </> ("input-" ++ prob) <.> "txt"
 
 linesFor :: String -> IO [String]
 linesFor = fmap lines . inputFor
+
+-- Parse input lines with the given parser, and log failiures.
+logParse :: (String -> Maybe a) -> [String] -> IO [a]
+logParse p l = do
+  let (failed, parsed) = partition (isNothing . snd . fst) $ zip [(s, p s) | s <- l] [1 ..]
+      format ((s, _), n) = "Failed to parse line " ++ show n ++ ": " ++ s
+
+  if null failed
+    then return $ map (fromJust . snd . fst) parsed
+    else mapM_ (putStrLn . format) failed >> fail "Failed to parse input"
