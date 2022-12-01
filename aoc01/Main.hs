2e43a2e90cdbf36f533cd513d5acffdd84452059
Elizabeth Paź <me@ehllie.xyz>
2022-12-01 08:22:32 +0100

    Add solution 01

--- aoc01/Main.hs
+++ aoc01/Main.hs
@@ -1,6 +1,25 @@
 module Main where
 
+import Data.List (sortBy)
 import Inputs (linesFor)
 
+splitEmpty :: Foldable f => [f a] -> [[f a]]
+splitEmpty [] = []
+splitEmpty xs =
+  let (ys, zs) = break null xs
+   in ys : splitEmpty (drop 1 zs)
+
+parse :: [String] -> [[Int]]
+parse = map (map read) . splitEmpty
+
+partOne :: [[Int]] -> Int
+partOne = maximum . map sum
+
+partTwo :: [[Int]] -> Int
+partTwo = sum . take 3 . sortBy (flip compare) . map sum
+
 main :: IO ()
-main = mapM_ putStrLn =<< linesFor "01"
+main = do
+  input <- parse <$> linesFor "01"
+  putStrLn $ "Part One: " ++ show (partOne input)
+  putStrLn $ "Part Two: " ++ show (partTwo input)
