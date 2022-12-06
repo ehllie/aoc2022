10873cffaa6fd3aeebdae4d57639539da6b99a4e
Elizabeth Paź <me@ehllie.xyz>
2022-12-06 08:29:06 +0100

    Add solution 06

--- aoc06/Main.hs
+++ aoc06/Main.hs
@@ -1,6 +1,28 @@
-module Main where
+module Main (main) where
 
-import Inputs (linesFor)
+import Control.Monad (foldM)
+import Data.List (sort)
+import Inputs (inputFor)
+
+uniqueChunk :: [Char] -> (Int, Char) -> Either Int [Char]
+uniqueChunk previous (index, current) =
+  let chunk = current : previous
+      sorted = sort chunk
+      allUnique = all (uncurry (>)) $ zip (tail sorted) sorted
+   in if allUnique then Left index else Right $ init chunk
+
+partOne :: String -> Either Int [Char]
+partOne stream = foldM uniqueChunk (reverse seed) $ zip [4 ..] rest
+ where
+  (seed, rest) = splitAt 3 stream
+
+partTwo :: String -> Either Int [Char]
+partTwo stream = foldM uniqueChunk (reverse seed) $ zip [14 ..] rest
+ where
+  (seed, rest) = splitAt 13 stream
 
 main :: IO ()
-main = mapM_ putStrLn =<< linesFor "06"
+main = do
+  input <- inputFor "06"
+  putStrLn $ "Part 1: " ++ show (partOne input)
+  putStrLn $ "Part 2: " ++ show (partTwo input)
