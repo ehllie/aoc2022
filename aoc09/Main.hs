f12fa3dd90c1bef0bcb0fd5d68c2908bc31aa8dc
Elizabeth Paź <me@ehllie.xyz>
2022-12-09 09:44:31 +0100

    Add solution 09

--- aoc09/Main.hs
+++ aoc09/Main.hs
@@ -1,6 +1,42 @@
-module Main where
+module Main (main) where
 
-import Inputs (linesFor)
+import Data.List.NonEmpty (NonEmpty (..))
+import Data.Set (Set, empty, insert, size)
+import Inputs (linesFor, logParse)
+import Text.Read (readMaybe)
+
+data Move = L | R | U | D
+  deriving (Show, Read)
+
+type Position = (Int, Int)
+
+type Visited = Set Position
+
+parse :: String -> Maybe [Move]
+parse s = case words s of
+  [dir, num] -> flip replicate <$> readMaybe dir <*> readMaybe num
+  _ -> Nothing
+
+updatePositions :: (Visited, NonEmpty Position) -> Move -> (Visited, NonEmpty Position)
+updatePositions (visited, hPos :| knots) move = (last knots' `insert` visited, hPos' :| knots')
+ where
+  hPos' = case move of
+    L -> (fst hPos - 1, snd hPos)
+    R -> (fst hPos + 1, snd hPos)
+    U -> (fst hPos, snd hPos + 1)
+    D -> (fst hPos, snd hPos - 1)
+  moveKnot h@(hX, hY) k@(kX, kY) = if adjacent h k then k else (kX + signum (hX - kX), kY + signum (hY - kY))
+  adjacent (x1, y1) (x2, y2) = abs (x1 - x2) <= 1 && abs (y1 - y2) <= 1
+  knots' = tail $ scanl moveKnot hPos' knots
+
+partOne :: [Move] -> Int
+partOne = size . fst . foldl updatePositions (empty, (0, 0) :| replicate 1 (0, 0))
+
+partTwo :: [Move] -> Int
+partTwo = size . fst . foldl updatePositions (empty, (0, 0) :| replicate 9 (0, 0))
 
 main :: IO ()
-main = mapM_ putStrLn =<< linesFor "09"
+main = do
+  input <- concat <$> (logParse parse =<< linesFor "09")
+  putStrLn $ "Part 1: " ++ show (partOne input)
+  putStrLn $ "Part 2: " ++ show (partTwo input)
